34d415760a4420bd01eadf2f17832b2db38194e4
zhang.jinshan <neusoft@600718>
2023-03-15 17:49:41 +0800

    auto commit

--- config-client/src/main/java/com/zjs/configclient/controller/ConfigClientController.java
+++ config-client/src/main/java/com/zjs/configclient/controller/ConfigClientController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author zjs
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 @RefreshScope
+@RequestMapping("/test")
 public class ConfigClientController {
     @Value("${app.env}")
     private String str;
 
     @GetMapping("/name")
+    @ResponseBody
     public String test() {
         return this.str;
     }
